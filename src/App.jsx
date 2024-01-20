f097e3b9c03a7202af4ca7c1b6252dd65b0340ba
Josh Shavers <shaversj@gmail.com>
2024-01-19 19:43:57 -0600

    Dark Mode Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import location from "/src/assets/icon-location.svg";
-import website from "/src/assets/icon-website.svg";
-import twitter from "/src/assets/icon-twitter.svg";
-import company from "/src/assets/icon-company.svg";
-import moon from "/src/assets/icon-moon.svg";
-import sun from "/src/assets/icon-sun.svg";
 import search from "/src/assets/icon-search.svg";
 import axios from "axios";
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 function App() {
-  const inputRef = useRef();
+  const [darkMode, setDarkMode] = useState(false);
+
   const [data, setData] = useState({
     login: "octocat",
     name: "The Octocat",
@@ -36,29 +31,40 @@ function App() {
       console.error(error);
     }
   };
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
     <>
-      <div className={"grid min-h-screen place-items-center bg-[#F6F8FF]"}>
+      <div className={`grid min-h-screen place-items-center ${darkMode ? "dark bg-[#141D2F]" : "bg-[#F6F8FF]"}`}>
         <div className={"font-SpaceMono max-w-screen-sm px-6 lg:min-w-[730px] lg:px-0"}>
           <header className={"flex"}>
-            <h1 className={"text-[26px] font-bold text-[#222731]"}>devfinder</h1>
+            <h1 className={"text-[26px] font-bold text-[#222731] dark:text-white"}>devfinder</h1>
             <div className={"ml-auto flex items-center justify-center gap-x-4"}>
-              <span className={"text-[13px] font-medium uppercase tracking-[2.5px] text-[#4B6A9B]"}>Dark</span>
-              <img src={moon} alt={""} />
+              <span className={"text-[13px] font-medium uppercase tracking-[2.5px] text-[#4B6A9B] dark:text-white"}>{darkMode ? "Dark" : "Light"}</span>
+              <button onClick={toggleDarkMode}>
+                <img src={darkMode ? "/src/assets/icon-sun.svg" : "/src/assets/icon-moon.svg"} alt={""} />
+              </button>
             </div>
           </header>
           <main>
-            <section className={"mt-9 rounded-2xl bg-white py-1.5 pl-3 lg:pl-8 lg:pr-2.5"}>
+            <section className={"mt-9 rounded-2xl bg-white py-1.5 pl-3 lg:pl-8 lg:pr-2.5 dark:bg-[#1E2A47]"}>
               <form className={"flex items-center justify-evenly lg:justify-start"} onSubmit={handleSubmit}>
                 <img className={"pr-6"} src={search} alt={""} />
-                <input onChange={(e) => setUserName(e.target.value)} className={"h-[25px] w-[184px] truncate text-[13px] text-[#4B6A9B] placeholder:text-[#4B6A9B] lg:w-[254px] lg:text-lg"} placeholder={"Search Github username.."} />
+                <input
+                  onChange={(e) => setUserName(e.target.value)}
+                  className={"h-[25px] w-[184px] truncate text-[13px] text-[#4B6A9B] placeholder:text-[#4B6A9B] lg:w-[254px] lg:text-lg dark:bg-[#1E2A47] dark:placeholder:text-[#FFFFFF] dark:placeholder:text-white"}
+                  placeholder={"Search Github username.."}
+                />
                 <button type={"submit"} className={"h-[46px] w-[84px] rounded-xl bg-[#0079ff] lg:ml-auto lg:h-[50px] lg:w-[106px]"}>
                   <span className={"text-sm font-bold text-white"}>Search</span>
                 </button>
               </form>
             </section>
 
-            <section className={"mt-5 grid grid-cols-[auto,auto] rounded-2xl bg-white px-6 pb-12 pt-4 lg:grid lg:grid-cols-[auto,1fr] lg:grid-rows-[auto,1fr] lg:gap-x-[37px] lg:p-12"}>
+            <section className={"mt-5 grid grid-cols-[auto,auto] rounded-2xl bg-white px-6 pb-12 pt-4 lg:grid lg:grid-cols-[auto,1fr] lg:grid-rows-[auto,1fr] lg:gap-x-[37px] lg:p-12 dark:bg-[#1E2A47]"}>
               <svg className={"lg:h-[117px] lg:w-[117px]"} width="70" height="70" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <circle cx="35" cy="35" r="35" fill="url(#pattern0)" />
                 <defs>
@@ -75,48 +81,71 @@ function App() {
               </svg>
               <div className={""}>
                 <div className={"lg:grid lg:grid-cols-2 lg:items-center"}>
-                  <h2 className={"font-bold lg:text-[26px]"}>{data.name ? data.name : ""}</h2>
+                  <h2 className={"font-bold lg:text-[26px] dark:text-[#FFFFFF]"}>{data.name ? data.name : ""}</h2>
                   <h3 className={"text-[13px] text-[#0079ff] lg:col-start-1 lg:text-base"}>@{data.login}</h3>
                   <div className={"lg:col-start-2 lg:row-start-1 lg:ml-auto"}>
-                    <span className={"text-[13px] text-[#697C9A] lg:text-[15px]"}>Joined {new Date("2011-01-25T18:44:36Z").toLocaleDateString("en-US", { day: "numeric", month: "short", year: "numeric" }).replace(/,/g, "")}</span>
+                    <span className={"text-[13px] text-[#697C9A] lg:text-[15px] dark:text-[#FFFFFF]"}>Joined {new Date("2011-01-25T18:44:36Z").toLocaleDateString("en-US", { day: "numeric", month: "short", year: "numeric" }).replace(/,/g, "")}</span>
                   </div>
                 </div>
               </div>
 
-              <p className={"pt-[33px] text-[13px] leading-[25px] text-[#4B6A9B] sm:col-span-full lg:col-start-2 lg:-mt-[80px]"}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros.</p>
+              <p className={"pt-[33px] text-[13px] leading-[25px] text-[#4B6A9B] sm:col-span-full lg:col-start-2 lg:-mt-[80px] dark:text-[#FFFFFF]"}>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros.
+              </p>
 
               <div className={"col-span-full lg:col-start-2"}>
-                <div className={"mt-[23px] flex justify-center gap-x-8 rounded-md bg-[#F6F8FF] py-[18px] lg:justify-start lg:gap-x-[100px] lg:pl-[32px]"}>
+                <div className={"mt-[23px] flex justify-center gap-x-8 rounded-md bg-[#F6F8FF] py-[18px] lg:justify-start lg:gap-x-[100px] lg:pl-[32px] dark:bg-[#141D2F]"}>
                   <div>
-                    <h4 className={"text-center text-[11px] text-[#4B6A9B] lg:text-[13px]"}>Repos</h4>
-                    <h4 className={"pt-[8px] text-center font-bold text-[#2B3442] lg:pt-[1px] lg:text-start lg:text-[22px]"}>{data.public_repos}</h4>
+                    <h4 className={"text-center text-[11px] text-[#4B6A9B] lg:text-[13px] dark:text-[#FFFFFF]"}>Repos</h4>
+                    <h4 className={"pt-[8px] text-center font-bold text-[#2B3442] lg:pt-[1px] lg:text-start lg:text-[22px] dark:text-[#FFFFFF]"}>{data.public_repos}</h4>
                   </div>
                   <div>
-                    <h4 className={"text-[11px] text-[#4B6A9B] lg:text-[13px]"}>Followers</h4>
-                    <h4 className={"pt-[8px] text-center font-bold text-[#2B3442] lg:pt-[1px] lg:text-start lg:text-[22px]"}>{data.followers}</h4>
+                    <h4 className={"text-[11px] text-[#4B6A9B] lg:text-[13px] dark:text-[#FFFFFF]"}>Followers</h4>
+                    <h4 className={"pt-[8px] text-center font-bold text-[#2B3442] lg:pt-[1px] lg:text-start lg:text-[22px] dark:text-[#FFFFFF]"}>{data.followers}</h4>
                   </div>
                   <div>
-                    <h4 className={"text-[11px] text-[#4B6A9B] lg:text-[13px]"}>Following</h4>
-                    <h4 className={"pt-[8px] text-center font-bold text-[#2B3442] lg:pt-[1px] lg:text-start lg:text-[22px]"}>{data.following}</h4>
+                    <h4 className={"text-[11px] text-[#4B6A9B] lg:text-[13px] dark:text-[#FFFFFF]"}>Following</h4>
+                    <h4 className={"pt-[8px] text-center font-bold text-[#2B3442] lg:pt-[1px] lg:text-start lg:text-[22px] dark:text-[#FFFFFF]"}>{data.following}</h4>
                   </div>
                 </div>
 
                 <div className={"pt-6 lg:grid lg:grid-cols-2 lg:place-items-baseline"}>
                   <div className={"flex items-center gap-x-4"}>
-                    <img src={location} alt={""} />
-                    <span className={"text-[13px] text-[#4B6A9B]"}>{data.location}</span>
+                    <svg height="20" width="20" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        className={"dark:fill-[#FFFFFF]"}
+                        d="M12.797 3.425C11.584 1.33 9.427.05 7.03.002a7.483 7.483 0 00-.308 0C4.325.05 2.17 1.33.955 3.425a6.963 6.963 0 00-.09 6.88l4.959 9.077.007.012c.218.38.609.606 1.045.606.437 0 .828-.226 1.046-.606l.007-.012 4.96-9.077a6.963 6.963 0 00-.092-6.88zm-5.92 5.638c-1.552 0-2.813-1.262-2.813-2.813s1.261-2.812 2.812-2.812S9.69 4.699 9.69 6.25 8.427 9.063 6.876 9.063z"
+                        fill="#4b6a9b"
+                      />
+                    </svg>
+                    <span className={"text-[13px] text-[#4B6A9B] lg:text-[15px] dark:text-[#FFFFFF]"}>{data.location}</span>
                   </div>
                   <div className={"flex items-center gap-x-4 pt-4 lg:order-2"}>
-                    <img src={website} alt={""} />
-                    <span className={"text-[13px] text-[#4B6A9B] lg:text-[15px]"}>{data.blog}</span>
+                    <svg height="20" width="20" xmlns="http://www.w3.org/2000/svg">
+                      <g fill="#4b6a9b" className={"dark:fill-[#FFFFFF]"}>
+                        <path d="M7.404 5.012c-2.355 2.437-1.841 6.482.857 8.273.089.06.207.048.283-.027.568-.555 1.049-1.093 1.47-1.776a.213.213 0 00-.084-.3A2.743 2.743 0 018.878 10.1a2.64 2.64 0 01-.223-1.803c.168-.815 1.043-1.573 1.711-2.274l-.004-.002 2.504-2.555a2.568 2.568 0 013.648-.019 2.6 2.6 0 01.037 3.666l-1.517 1.56a.266.266 0 00-.06.273c.35 1.012.435 2.44.201 3.519-.006.03.031.05.053.028l3.228-3.295c2.062-2.105 2.044-5.531-.04-7.615a5.416 5.416 0 00-7.691.04L7.417 4.998l-.013.014z" />
+                        <path d="M13.439 13.75a.401.401 0 00.006-.003c.659-1.204.788-2.586.48-3.933l-.002.002-.001-.001a5.434 5.434 0 00-2.19-3.124.3.3 0 00-.333.015c-.553.448-1.095 1.021-1.452 1.754a.243.243 0 00.096.317c.415.24.79.593 1.04 1.061h.001c.196.33.388.958.263 1.632-.116.894-1.019 1.714-1.736 2.453-.546.559-1.935 1.974-2.49 2.542a2.6 2.6 0 01-3.666.037 2.6 2.6 0 01-.038-3.666l1.521-1.564A.266.266 0 005 11.004c-.338-1.036-.43-2.432-.217-3.51.006-.03-.031-.049-.053-.027l-3.179 3.245c-2.083 2.126-2.066 5.588.04 7.693 2.125 2.083 5.57 2.048 7.653-.078.723-.81 3.821-3.678 4.195-4.577z" />
+                      </g>
+                    </svg>
+                    <span className={"text-[13px] text-[#4B6A9B] lg:text-[15px] dark:text-[#FFFFFF]"}>{data.blog}</span>
                   </div>
                   <div className={"flex items-center gap-x-4 pt-4"}>
-                    <img src={twitter} alt={""} />
-                    <span className={"text-[13px] text-[#4B6A9B] lg:text-[15px]"}>@{data.twitter_username}</span>
+                    <svg height="18" width="20" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M20 2.799a8.549 8.549 0 01-2.363.647 4.077 4.077 0 001.804-2.266 8.194 8.194 0 01-2.6.993A4.099 4.099 0 009.75 4.977c0 .324.027.637.095.934-3.409-.166-6.425-1.8-8.452-4.288a4.128 4.128 0 00-.56 2.072c0 1.42.73 2.679 1.82 3.408A4.05 4.05 0 01.8 6.598v.045a4.119 4.119 0 003.285 4.028 4.092 4.092 0 01-1.075.135c-.263 0-.528-.015-.776-.07.531 1.624 2.038 2.818 3.831 2.857A8.239 8.239 0 01.981 15.34 7.68 7.68 0 010 15.285a11.543 11.543 0 006.29 1.84c7.545 0 11.67-6.25 11.67-11.667 0-.182-.006-.357-.015-.53A8.18 8.18 0 0020 2.798z"
+                        fill="#4b6a9b"
+                        className={"dark:fill-[#FFFFFF]"}
+                      />
+                    </svg>
+                    <span className={"text-[13px] text-[#4B6A9B] lg:text-[15px] dark:text-[#FFFFFF]"}>{data.twitter_username ? "@" + data.twitter_username : "Not Available"}</span>
                   </div>
                   <div className={"flex items-center gap-x-4 pt-4 lg:order-4"}>
-                    <img src={company} alt={""} />
-                    <span className={"text-[13px] text-[#4B6A9B] lg:text-[15px]"}>{data.company}</span>
+                    <svg height="20" width="20" xmlns="http://www.w3.org/2000/svg">
+                      <g fill="#4b6a9b" className={"dark:fill-[#FFFFFF]"}>
+                        <path d="M10.858 1.558L1.7.167A1.477 1.477 0 00.517.492 1.49 1.49 0 000 1.608v17.559c0 .458.375.833.833.833h2.709v-4.375c0-.808.65-1.458 1.458-1.458h2.083c.809 0 1.459.65 1.459 1.458V20h3.541V3a1.46 1.46 0 00-1.225-1.442zM4.583 12.292h-1.25a.625.625 0 010-1.25h1.25a.625.625 0 010 1.25zm0-2.5h-1.25a.625.625 0 010-1.25h1.25a.625.625 0 010 1.25zm0-2.5h-1.25a.625.625 0 010-1.25h1.25a.625.625 0 010 1.25zm0-2.5h-1.25a.625.625 0 010-1.25h1.25a.625.625 0 010 1.25zm4.167 7.5H7.5a.625.625 0 010-1.25h1.25a.625.625 0 010 1.25zm0-2.5H7.5a.625.625 0 010-1.25h1.25a.625.625 0 010 1.25zm0-2.5H7.5a.625.625 0 010-1.25h1.25a.625.625 0 010 1.25zm0-2.5H7.5a.625.625 0 010-1.25h1.25a.625.625 0 010 1.25zM18.85 9.035l-5.933-1.242V20h5.625A1.46 1.46 0 0020 18.542V10.46c0-.688-.47-1.274-1.15-1.425zM16.875 17.5h-1.25a.625.625 0 010-1.25h1.25a.625.625 0 010 1.25zm0-2.5h-1.25a.625.625 0 010-1.25h1.25a.625.625 0 010 1.25zm0-2.5h-1.25a.625.625 0 010-1.25h1.25a.625.625 0 010 1.25z" />
+                      </g>
+                    </svg>
+                    <span className={"text-[13px] text-[#4B6A9B] lg:text-[15px] dark:text-[#FFFFFF]"}>{data.company}</span>
                   </div>
                 </div>
               </div>
